f5a13cffa114921537bad5d0844bfe65e5309567
Ameya Kulkarni <ameyak2001@gmail.com>
2024-08-05 21:49:13 +0530

    user login-logout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 
 import DashBoard from "./pages/Dashboard";
 import Bookings from "./pages/Bookings";
@@ -16,6 +16,7 @@ import Booking from "./pages/Booking";
 import { GlobalStyles } from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
 import CheckIn from "./pages/CheckIn";
+import ProtectedRoute from "./ui/ProtectedRoute";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -31,12 +32,18 @@ function App() {
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route element={<AppLayout />}>
+          <Route
+            element={
+              <ProtectedRoute>
+                <AppLayout />
+              </ProtectedRoute>
+            }
+          >
             <Route path="dashboard" element={<DashBoard />} />
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="bookings" element={<Bookings />} />
-            <Route path='bookings/:bookingId' element={<Booking />}/>
-            <Route path='checkin/:bookingId' element={<CheckIn />}/>
+            <Route path="bookings/:bookingId" element={<Booking />} />
+            <Route path="checkin/:bookingId" element={<CheckIn />} />
             <Route path="cabins" element={<Cabins />} />
             <Route path="users" element={<Users />} />
             <Route path="settings" element={<Settings />} />
