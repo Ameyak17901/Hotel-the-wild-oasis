f6a9a2fa9c07a8c8751a044a647d0a3d9b66f3b4
Ameya Kulkarni <ameyak2001@gmail.com>
2024-08-07 18:36:55 +0530

    signup feature complete

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -7,8 +7,8 @@ import { useLogin } from "./useLogin";
 import SpinnerMini from "../../ui/SpinnerMini";
 
 function LoginForm() {
-  const [email, setEmail] = useState("ameyak2001@gmail.com");
-  const [password, setPassword] = useState("Ameya@17");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { login, isLoading } = useLogin();
 
