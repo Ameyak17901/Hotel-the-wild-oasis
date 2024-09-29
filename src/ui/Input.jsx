682d79a7fe20973bad2841fc2c16872bedf166ee
Ameya Kulkarni <ameyak2001@gmail.com>
2024-09-29 11:31:37 +0530

    minor fixes in UI

--- src/ui/Input.jsx
+++ src/ui/Input.jsx
@@ -1,9 +1,10 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 const Input = styled.input`
   border: 1px solid var(--color-grey-300);
   border-radius: var(--border-radius-sm);
   padding: 0.2rem 1.2rem;
-`
+  background-color: var(--backdrop-color);
+`;
 
-export default Input
+export default Input;
